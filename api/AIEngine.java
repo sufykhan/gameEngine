a0ff5bda4a6e2ffb7f81a130ea9e849dd3c60403
Sufyan <sufyan.khan@getvymo.com>
2025-02-26 00:25:33 +0530

    SUF-12 Good Engineer1: Adding Game Info and writing different Optimal Moves functions

--- api/AIEngine.java
+++ api/AIEngine.java
@@ -4,35 +4,20 @@ import boards.TicTacToeBoard;
 import game.*;
 
 public class AIEngine {
+    private RuleEngine ruleEngine;
 
-    public Move getBasicMove(Player player, TicTacToeBoard board){
+    public Cell getBasicMove(Player player, TicTacToeBoard board){
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board.getSymbol(i,j)==null){
-                    return new Move(player,new Cell(i,j));
+                    return new Cell(i,j);
                 }
             }
         }
         return null;
     }
 
-    public Move getSmartMove(Player player, TicTacToeBoard board){
-        RuleEngine ruleEngine = new RuleEngine();
-        // Victorious Move = We found a move where Computer Wins
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board.getSymbol(i,j)==null){
-                    Move move = new Move(player, new Cell(i,j));
-                    Board boardCopy = board.clone();
-                    boardCopy.move(move);
-                    if(ruleEngine.getState(boardCopy).isOver()){
-                        return move;
-                    }
-                }
-            }
-        }
-
-        // Defensive Move = We found a move where Player Wins and we replace it with computer move
+    private Cell offense(Player player, TicTacToeBoard board) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board.getSymbol(i,j)==null){
@@ -41,30 +26,89 @@ public class AIEngine {
                     Board boardCopy = board.clone();
                     boardCopy.move(move);
                     if(ruleEngine.getState(boardCopy).isOver()){
-                        return  new Move(player, new Cell(i,j));
+                        return  new Cell(i,j);
                     }
                 }
             }
         }
+        return null;
+    }
+
+    private Cell defense(Player player, TicTacToeBoard board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board.getSymbol(i, j) == null) {
+                    Move move = new Move(player.flip(), new Cell(i,j));
+                    Board boardCopy = board.clone();
+                    boardCopy.move(move);
+                    if (ruleEngine.getState(boardCopy).isOver()) {
+                        return new Cell(i,j);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Cell getSmartMove(Player player, TicTacToeBoard board){
+        // Victorious Move = We found a move where Computer Wins
+        Cell defense = defense(player,board);
+        if(defense!=null) return defense;
+
+        Cell offense = offense(player,board);
+        if(offense!=null) return offense;
+
         return getBasicMove(player, board);
+    }
+
+    public Cell getOptimalMove(Player player, TicTacToeBoard board){
+
+        // 1. Victorious Move = We found a winning move.
+        Cell best = defense(player,board);
+        if(best!=null) return best;
+        // 2. Defensive Move = We found a move where Player wins and we block the move.
+        best = offense(player,board);
+        if(best!=null) return best;
+        // 3. Fork Move = If I have a fork, then play it.
+        // 4. AntiFork Move = If opponent have a fork, then block it.
+
+        GameInfo gameInfo = ruleEngine.getInfo(board);
+        if(gameInfo.hasAFork()){
+            best = gameInfo.getForkCell();
+            return best;
+        }
+        // 5. If the center is available, play it.
+        if(board.getSymbol(1,1)==null) return new Cell(1,1);
+        // 6. If the corner is available, play it.
+        int corners[][] =  new int[][]{{0,0},{0,2},{2,0},{2,2}};
+        for(int i=0;i<4;i++){
+            if(board.getSymbol(corners[i][0],corners[i][1])==null) return new Cell(corners[i][0],corners[i][1]);
+        }
+
+        return getBasicMove(player, board);
+
+        //return new Cell(0,0);
     }
 
     public Move suggestMove(Player player, Board board){
         if(board instanceof TicTacToeBoard board1){
-            Move suggestedMove = null;
-            if(hasLessMove(board1,2)){
+            Cell suggestedMove = null;
+            int threshold = 2;
+            if(countMoves(board1) < threshold){
                 suggestedMove = getBasicMove(player,board1);
-            } else {
+            } else if(countMoves(board1)< threshold + 1) {
                 suggestedMove = getSmartMove(player,board1);
+            } else{
+                suggestedMove = getOptimalMove(player, board1);
             }
-            if(suggestedMove != null) return suggestedMove;
+            if(suggestedMove != null) return new Move(player,suggestedMove);
             throw new IllegalStateException();
         } else{
             throw new IllegalArgumentException();
         }
     }
 
-    public boolean hasLessMove(TicTacToeBoard board, int threshHold) {
+    public int countMoves(TicTacToeBoard board) {
             int count = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -73,6 +117,6 @@ public class AIEngine {
                     }
                 }
             }
-            return count<threshHold;
+            return count;
     }
 }
--- api/RuleEngine.java
+++ api/RuleEngine.java
@@ -1,11 +1,12 @@
 package api;
 
 import boards.TicTacToeBoard;
-import game.Board;
-import game.GameState;
+import game.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
 public class RuleEngine {
     Map<String, RuleSet> ruleMap = new HashMap<>();
 
@@ -24,6 +25,51 @@ public class RuleEngine {
             return new GameState(false,"-");
         } else {
             return new GameState(true, "-");
+        }
+    }
+
+    public GameInfo getInfo(Board board) {
+        if (board instanceof TicTacToeBoard) {
+            TicTacToeBoard ticTacToeBoard = (TicTacToeBoard) board;
+            GameState gameState = getState(ticTacToeBoard);
+            final String [] players = new String[] {"X","O"};
+            Cell forkCell = null;
+            for (int index =0; index <2 ;index++) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        Board b = ((TicTacToeBoard) board).clone();
+                        Player player = new Player(players [index]);
+                        b.move(new Move(player,new Cell(i,j)));
+                        boolean canStillWin = false;
+                        for (int k = 0; k < 3; k++) {
+                            for (int l = 0; l < 3; l++) {
+                               Board b1 = ((TicTacToeBoard) b).clone();
+                               forkCell= new Cell(k,l);
+                               b1.move(new Move(player.flip(),new Cell(k,l)));
+                               if(getState(b1).getWinner().equals(player.flip().symbol())){
+                                   canStillWin = true;
+                                   break;
+                               }
+                            }
+                        }
+                        if(canStillWin) {
+                            return new GameInfoBuilder()
+                                    .isOver(gameState.isOver())
+                                    .winner(gameState.getWinner())
+                                    .hasFork(true)
+                                    .player(player.flip())
+                                    .forkCell(forkCell)
+                                    .build();
+                        }
+                    }
+                }
+            }
+            return new GameInfoBuilder()
+                    .isOver(gameState.isOver())
+                    .winner(gameState.getWinner())
+                    .build();
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
